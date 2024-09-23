79b57310b91fdb3a0ac51ca77a0ba8aae971c16b
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-09-23 21:45:37 +1000

    Landing curved shape draft implementation

--- app/_custom_components/Landing.tsx
+++ app/_custom_components/Landing.tsx
@@ -3,15 +3,20 @@ import LandingMontage from "./LandingMontage";
 import EcoscopeButton from "./Button";
 import Container from "./Container";
 import { H1 } from "./Headings";
+import Image from "next/image";
 
-type Props = {};
-
-const Landing = (props: Props) => {
+const Landing = () => {
   return (
     <>
       <LandingMontage />
-      <Container className="flex flex-col justify-center video-overlay">
-        <div className="w-full md:w-fit bg-transparent-white p-7 backdrop-blur rounded-lg">
+      {/* <Image
+        src="/_assets/curve-background.png"
+        alt="curve"
+        height={800}
+        width={600}
+      /> */}
+      <div className="flex flex-col justify-end video-overlay relative">
+        {/* <div className="w-full md:w-fit bg-transparent-white p-7 backdrop-blur rounded-lg">
           <div className="table">
             <H1 className="text-left m-0 mb-4">
               Your Guide to <br className="hidden md:block" />
@@ -31,20 +36,32 @@ const Landing = (props: Props) => {
               Contact Us
             </EcoscopeButton>
           </div>
-          {/* <h2 className="mb-6">
-            Empowering businesses with trusted sustainability certifications for
-            environmental responsibility and a greener, more sustainable future.
-          </h2>
-          <div className="flex flex-col md:flex-row justify-between space-y-3 md:space-x-6 md:space-y-0">
-            <EcoscopeButton href="about-us" className="w-full">
-              Our Services
-            </EcoscopeButton>
-            <EcoscopeButton href="contact-us" className="w-full">
-              Contact Us
-            </EcoscopeButton>
-          </div> */}
-        </div>
-      </Container>
+        </div> */}
+        <div className="bg-curve absolute bottom-0 left-0 w-full h-full" />
+        <Container className="absolute bottom-12 left-0">
+          <div className="w-full md:w-fit">
+            <div className="table">
+              <H1 className="text-left m-0 mb-4">
+                Your Guide to <br />
+                Greener Productions
+              </H1>
+              <h2 className="mb-6 md:table-caption md:caption-bottom">
+                Empowering businesses with trusted sustainability certifications
+                for environmental responsibility and a greener, more sustainable
+                future.
+              </h2>
+            </div>
+            <div className="flex flex-col sm:flex-row justify-between space-y-3 sm:space-x-6 sm:space-y-0">
+              <EcoscopeButton href="#services" className="w-full">
+                Our Services
+              </EcoscopeButton>
+              <EcoscopeButton href="contact-us" className="w-full">
+                Contact Us
+              </EcoscopeButton>
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
