f39cf61f4930ad39b4b83c4c20ef7e08eb9fad6a
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-11-06 12:18:51 +1100

    Numbers added to About Us section

--- app/_custom_components/About.tsx
+++ app/_custom_components/About.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import EcoscopeButton from "./Button";
 import Container from "./Container";
 import { H3 } from "./Headings";
 
@@ -41,10 +40,15 @@ const About = () => {
     <Container className="flex flex-col justify-center py-32">
       <H3 className="text-center">What We Do</H3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-12 lg:gap-x-16 xl:gap-x-40 gap-y-10 lg:gap-y-24">
-        {aboutValues.map((value) => {
+        {aboutValues.map((value, i) => {
           return (
             <div>
-              <h6 className="text-2xl">{value.name}</h6>
+              <h6 className="text-2xl">
+                <span className="text-primary text-2xl opacity-70 mr-3">
+                  0{i + 1}
+                </span>
+                {value.name}
+              </h6>
               <hr className="h-1 primary-gradient my-3" />
               <p className="text-base">{value.description}</p>
             </div>
