043db366027af5e79cc26d28ef1f13e54e14bef7
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-11-06 15:09:54 +1100

    Bones of form structure started

--- app/_custom_components/BookingForm.tsx
+++ app/_custom_components/BookingForm.tsx
@@ -2,22 +2,210 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { formSchema } from "./FormSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { H3 } from "./Headings";
 
 const BookingForm = () => {
   const [loading, setLoading] = useState<Boolean>(true);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: 0,
+      companyName: "",
+      industry: "Film",
+      companySize: "0-10",
+      message: "",
+    },
+  });
 
   const handleLoading = () => {
     setLoading(false);
   };
 
   return (
-    <>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" id="email" placeholder="Email" />
-      </div>
-    </>
+    <Form {...form}>
+      <form className="flex-1 grid sm:grid-cols-2 sm:gap-x-10 p-10">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" id="name" placeholder="Name" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" id="email" placeholder="Email" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone</FormLabel>
+              <FormControl>
+                <Input type="tel" id="phone" placeholder="Phone" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="companyName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company Name</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  id="company-name"
+                  placeholder="Company Name"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Industry</FormLabel>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Industry" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="film">Film</SelectItem>
+                  <SelectItem value="commercial">Commercial</SelectItem>
+                  <SelectItem value="hospitality">Hospitality</SelectItem>
+                  <SelectItem value="other">Other</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Certification</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  id="certification"
+                  placeholder="Certification"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company Size</FormLabel>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Company Size" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="0-10">0-10</SelectItem>
+                  <SelectItem value="11-50">11-50</SelectItem>
+                  <SelectItem value="51-200">51-200</SelectItem>
+                  <SelectItem value="201-500">201-500</SelectItem>
+                  <SelectItem value="501-1000">501-1000</SelectItem>
+                  <SelectItem value="1001-5000">1001-5000</SelectItem>
+                  <SelectItem value="5001-10000">5001-10,000</SelectItem>
+                  <SelectItem value="10001+">10,000 +</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  id="message"
+                  placeholder="Message"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   );
 };
 
---
+++ app/_custom_components/FormSchema.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { z } from "zod";
+
+export const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email().min(2).max(50),
+  phone: z.number().min(2).max(50),
+  companyName: z.string().min(2).max(50),
+  industry: z.enum(["Film", "Commercial", "Hospitality", "Other"]),
+  certification: z.string().min(2).max(50),
+  companySize: z.enum([
+    "0-10",
+    "11-50",
+    "51-200",
+    "201-500",
+    "501-1000",
+    "1001-5000",
+    "5001-10000",
+    "10001+",
+  ]),
+  message: z.string().min(2).max(500),
+});
