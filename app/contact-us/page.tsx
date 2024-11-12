949e02da0fc0a172fac4eb692712232580bb6e62
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-11-12 20:02:19 +1100

    Typo

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -17,7 +17,7 @@ const ContactUs = () => {
             <H4
               mb
               subtitle="Please fill out the form below if you're interested in finding
-              out more about what Ecoscope can do for yoru business"
+              out more about what Ecoscope can do for your business"
             >
               Book a Consultation
             </H4>
