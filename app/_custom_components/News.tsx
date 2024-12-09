69302c1b7f9dc7da86cf3868de4620414b7b471e
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-12-10 08:06:18 +1100

    Removal of button to NEes page for now

--- app/_custom_components/News.tsx
+++ app/_custom_components/News.tsx
@@ -7,17 +7,13 @@ import EcoscopeButton from "./Button";
 const News = () => {
   return (
     <Container mb className="px-0 flex flex-col justify-center text-center">
-      <div>
-        <H2
-          mb
-          subtitle="Get insights into our company and the ever-growing industry of sustainability in business"
-        >
-          Explore our News
-        </H2>
-        <MoreStories params={{ skip: "", limit: 3, industry: "" }} />
-
-        <EcoscopeButton href="/news">Read More</EcoscopeButton>
-      </div>
+      <H2
+        mb
+        subtitle="Get insights into our company and the ever-growing industry of sustainability in business"
+      >
+        Explore our News
+      </H2>
+      <MoreStories params={{ skip: "", limit: 3, industry: "" }} />
     </Container>
   );
 };
